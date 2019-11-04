f8a1b6f1843df92a8d81884819990f70d5671772
Haoda Wang <harry@h313.info>
2019-11-04 12:46:16 -0800

    add skeleton for analyzing a connection

--- consumer/leader_consumer.cpp
+++ consumer/leader_consumer.cpp
@@ -1,2 +1,10 @@
 #include "leader_consumer.h"
-#include <
+#include <python3.7m/Python.h>
+
+float analyze_conn(const std::string &in) {
+  Py_SetProgramName(
+      reinterpret_cast<const wchar_t *>("leader_ellipticenvelope"));
+  Py_Initialize();
+
+
+}
--- consumer/leader_consumer.h
+++ consumer/leader_consumer.h
@@ -1,8 +1,12 @@
 #ifndef LEADER_CONSUMER_H
 #define LEADER_CONSUMER_H
+#include <string>
 
-class leader_consumer {
-
-};
+/**
+ * Checks if a connection is an attack or not
+ * @param in CSV string from leader_session
+ * @return chance of attack from 0 to 1
+ */
+float analyze_conn(const std::string& in);
 
 #endif //LEADER_CONSUMER_H
