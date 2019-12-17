ab10c884680570f760c6cbb41a89971a8f170d58
rajattan <rajattan@usc.edu>
2019-12-17 15:06:21 -0700

    Modified setup

--- experiments/setup/setup.sh
+++ experiments/setup/setup.sh
@@ -5,4 +5,3 @@ bash assignlegitimate
 bash assignattackers
 ssh $1.$experiment_name.leader "bash $path_to_execute_from/experiments/setup/setup_server.sh $experiment_name $path_to_execute_from"
 # setup attackers
-bash tune-all.sh $1
