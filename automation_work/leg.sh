0896e9c29d7fb1d4b53ebecfd4f9cfa0e3fc93d0
rajattan <rajattan@usc.edu>
2021-06-10 12:30:24 -0600

    leg.sh

---
+++ automation_work/leg.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo apparmor_parser -R /etc/apparmor.d/usr.sbin.tcpdump 
+sudo pip3 install netifaces
+sudo pip3 install python-dateutil
+sudo pip3 install shove
