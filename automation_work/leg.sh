e3f8d42f06491887ec7dd3cd20356fb950434834
rajattan <rajattan@usc.edu>
2021-06-10 12:49:49 -0600

    leg.sh

--- automation_work/leg.sh
+++ automation_work/leg.sh
@@ -7,3 +7,4 @@ sudo pip3 install shove
 #cd ~/frade/traffic/smart_attacker/
 #sudo bash install
 cd ~/frade//experiments/setup/ip_addrs/; sudo perl assign 10.1.x.x
+#python3 legitimate.py -s 10.1.1.2 --sessions 100 --logs /proj/Leader/access.log --interface enp6s0f1
